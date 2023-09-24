810a982559251920cdd1f790a0c01c7ea6fa2f17
ArunR4 <arunramanmurthy@gmail.com>
2023-09-24 12:06:31 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,16 @@
+function appendInput(number) {
+			inputElement = document.getElementById("txtbox-1");
+			inputElement.value += number;
+		}
+
+		function calculateResult() {
+			var inputElement = document.getElementById("txtbox-1");
+			var expression = inputElement.value;
+			var result = eval(expression); 
+			inputElement.value = result;
+		}
+
+		function clearInput() {
+			var inputElement = document.getElementById("txtbox-1");
+			inputElement.value = '';
+		}
